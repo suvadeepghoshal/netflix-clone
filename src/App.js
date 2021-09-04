6cdc19e4f2df121377438f2dd54323835ed5c7ae
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-09-04 15:54:35 +0530

    intiating states to hold the movies from the api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
       <h1>This is the netflix clone</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="TRENDING NOW" />
     </div>
   );
 }
