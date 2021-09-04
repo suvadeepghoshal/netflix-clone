a28098d10d4ed6e6bd495f6acc4f03dde0a8bb77
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-09-04 17:06:31 +0530

    added all the required row components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Row from './components/Row';
+import requests from './service/request';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is the netflix clone</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="TRENDING NOW" />
+      <h1>NETFLIX</h1>
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="TOP RATED" fetchUrl={requests.fetchtopRated} />
+      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="ROMATIC MOVIES" fetchUrl={requests.fetchRomaticMovies} />
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
