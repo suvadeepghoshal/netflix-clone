72efe7f51f803011e9b3ee4c388ac2914fd489cb
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-09-04 16:01:09 +0530

    imported useState and removed redundate comment

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 /**
  * This function is one row component for the netflix website
  * Inside one row multiple movies will be displayed
@@ -6,8 +7,8 @@ import React from 'react'
  * @returns Row cmoponent
  */
 function Row(props) {
-    {/* States: short time memory to intialise the movies array */ }
-    const [movies, setMovies] = useState([]); // intially it is going to be an empty array
+    /* States: short time memory to intialise the movies array */
+    const [movies, setMovies] = useState([]); /* intially it is going to be an empty array */
     return (
         <div>
             <h2>{props.title}</h2>
