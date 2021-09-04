59abe52d73654d17585fbef6cbfc020b7aa4805d
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-09-04 16:41:32 +0530

    we are fetching the json response from the api

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
+import instance from '../utility/axios';
+import { useEffect } from 'react';
 /**
  * This function is one row component for the netflix website
  * Inside one row multiple movies will be displayed
@@ -9,6 +11,21 @@ import { useState } from 'react';
 function Row(props) {
     /* States: short time memory to intialise the movies array */
     const [movies, setMovies] = useState([]); /* intially it is going to be an empty array */
+    /* When the Row loads we need to load the movie information */
+    useEffect(() => {
+        /**
+         * This is asynchronous function to get the movies data
+         */
+        async function fetchData() {
+            let fetchURL = props.fetchUrl;
+            const request = instance.get(fetchURL);
+            console.log(request);
+        }
+        fetchData();
+        // return () => {
+        //     cleanup
+        // }
+    }, [movies])
     return (
         <div>
             <h2>{props.title}</h2>
