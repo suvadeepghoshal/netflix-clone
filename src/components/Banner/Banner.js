45bf69b0d6d06cc5722146352c8032578dba43c5
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-09-05 16:42:53 +0530

    added the banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div className="Banner">
+            This is the NETFLIX Banner
+        </div>
+    )
+}
+
+export default Banner
