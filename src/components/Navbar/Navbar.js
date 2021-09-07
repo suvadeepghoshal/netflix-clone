5b5d108e560709f84178e280fc8d5e2752ac9c9d
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-09-07 08:24:00 +0530

    created the navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <p>This is the navbar component</p>
+        </div>
+    )
+}
+
+export default Navbar
