ad92e0619c1e2eff94cdf6dc1709e1a3afb66962
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-09-07 09:05:05 +0530

    added scroll listener for fadin logic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import "./Navbar.css"
-
+import { useState } from 'react';
+import { useEffect } from 'react';
+/**
+ * 
+ * @returns Navbar component
+ */
 function Navbar() {
-    let netflixLogo = "https://assets.brand.microsites.netflix.io/assets/1ed15bca-b389-11e7-9274-06c476b5c346_cm_800w.png?v=60";
+    const [show, setShow] = useState(false);
+    /**
+     * If there is a scroll in the y axis > 100px, then we set a flag to true
+     * Otherwise the flag remains false always
+     * Based on the flag we will control when to show the black navbar and when not.
+     */
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setShow(true);
+            } else {
+                setShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, [])
+    let netflixLogo = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png";
     return (
-        <div className="Navbar">
+        // Here we will do the check for the navbar
+        <div className={`Navbar ${show == true && "showNavbar"}`}>
             <img src={netflixLogo} alt="Netflix Logo" className="Navbar__netflixLogo" />
         </div>
     )
