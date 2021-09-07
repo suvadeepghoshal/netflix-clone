08fb19f04f1b7710a810212b7fdbaa9375ae86a2
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-09-07 08:41:08 +0530

    added netflix logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./Navbar.css"
 
 function Navbar() {
+    let netflixLogo = "https://assets.brand.microsites.netflix.io/assets/1ed15bca-b389-11e7-9274-06c476b5c346_cm_800w.png?v=60";
     return (
         <div className="Navbar">
-            <p>This is the navbar component</p>
+            <img src={netflixLogo} alt="Netflix Logo" className="Navbar__netflixLogo" />
         </div>
     )
 }
